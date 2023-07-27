fa81a2a168a1920d14aa31e1e4530a6edab22591
KayaanPro <103744889+KayaanPro@users.noreply.github.com>
2023-07-27 13:56:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/VvsNvkTH/Clown-Nose-removebg-preview.png');
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){
+}
